4ad6c33165a912346ce05a495b808ba9f0802908
Nicos Maris <nicos.maris@gmail.com>
2016-12-28 01:05:00 +0200

    refactored

---
+++ lib/ssh2vm.py
@@ -0,0 +1,39 @@
+from delorean import Delorean
+
+from subprocess import Popen
+
+from fabric.api import env
+from fabric.operations import run, put
+
+class SSH2VM:
+    def __init__(self, ip):
+        self.ip = ip
+        env.hosts = [ip]
+        env.user = 'root'
+        env.key_filename = 'key'
+    def is_reachable(self):
+        result = false
+        command = 'date'
+        array = ["ssh",
+               "-i", "key",
+               "-o", "StrictHostKeyChecking=no",
+               "-o", "KbdInteractiveDevices=no",
+               "-o", "BatchMode=yes",
+               "%s@%s" % ('root', self.ip)]
+        array.append(command.split(' '))
+
+        first_atttempt = Delorean()
+        while True:
+            if Delorean() - first_attempt < timedelta(minutes=5):
+	        sleep(10)
+            else:
+                pid = Popen(array, stdout=PIPE, stderr=PIPE)
+                out, err = pid.communicate()                         # executes command
+                if pid.returncode==0:
+                    result = true
+                    break
+        return result
+    def copy(local_path):
+       put(local_path, '')
+
+
