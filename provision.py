7d07c1a7543948cbe2307eb7bd2d135486f80aa2
Nicos Maris <nicos.maris@gmail.com>
2016-12-29 02:14:37 +0200

    refactored

--- provision.py
+++ provision.py
@@ -2,21 +2,36 @@ from lib.vultr import Server
 from lib.ssh2vm import SSH2VM
 
 from requests import post, get
+from time import sleep
+
+class Provisioner:
+    srv = None
+    vm = None
+    label = None
+    def _init__(self, label):
+        self.label = label
+        self.srv = Server()
+        ip = self.srv.create(label)
+        try:
+            self.vm = SSH2VM(ip)
+            assert self.vm.is_reachable(), "VM is not reachable with ssh"
+            print('is reachable')
+            self.vm.upload("deploy/%s-install.sh" % label)
+            self.vm.upload("deploy/%s-entrypoint.sh" % label)
+            self.vm.execute("bash +x %s-install.sh" % label)
+        finally:
+            self.srv.destroy()
+    def start():
+        try:
+            self.vm.daemon("bash +x %s-entrypoint.sh" % self.label)
+            sleep(4)
+            print get("http://%s:8080" % ip).text
+        finally:
+            self.srv.destroy()
+
 
 def main():
-    s = Server()
-    ip = s.create('travis')
-    try:
-        vm = SSH2VM(ip)
-        assert vm.is_reachable(), "VM is not reachable with ssh"
-        print('is reachable')
-        vm.upload('deploy/smsc-install.sh')
-        vm.upload('deploy/smsc-entrypoint.sh')
-        vm.execute('bash +x smsc-install.sh')
-        vm.deamon('bash +x smsc-entrypoint.sh')
-        print get("http://%s:8080" % ip).text
-    finally:
-        s.destroy()
+    Provisioner('smsc').start()
 
 
 if __name__ == "__main__":
