c2f65a88ba4820514deb277411d1f548df28709e
Nicos Maris <nicos.maris@gmail.com>
2017-01-10 22:03:50 +0200

    ufw

--- deploy/smsc.sh
+++ deploy/smsc.sh
@@ -11,10 +11,12 @@ ufw allow 443/tcp  # HTTPS
 ufw allow 2376/tcp # docker daemon
 ufw allow 3376/tcp # Swarm API
 ufw allow 4789/udp # VXLAN
+ufw allow 4243/tcp
+sed -i 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/' /etc/default/ufw 
 ufw --force enable
 
-groupadd docker
-usermod -aG docker root
+#groupadd docker
+#usermod -aG docker root
 curl -sSL https://get.docker.com/ | sh
 
 echo "finished smsc.sh" >> /log.txt
