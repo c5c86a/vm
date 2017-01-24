b0dfe01d66e1a9cb96ef32ffbb47d015112f4dbd
Nicos Maris <nicos.maris@gmail.com>
2017-01-25 00:00:52 +0200

    kill apt from vultr

--- deploy/smsc.sh
+++ deploy/smsc.sh
@@ -7,8 +7,10 @@ killall -we 'apt-get'
 echo "start searching for 'apt-get -qq -y update' initiated by vultr..."
 ps aux | grep '[a]pt'
 echo "stop searching for 'apt-get -qq -y update' initiated by vultr..."
-#rm /var/cache/apt/archives/lock
+rm /var/lib/apt/lists/lock
+rm /var/cache/apt/archives/lock
 #rm /var/lib/dpkg/lock
+#dpkg --configure -a
 apt-get update
 
 ufw default deny incoming
