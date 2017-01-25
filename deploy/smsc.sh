c215afc852c758c3fbfc64ce5175ec393784943a
Nikos Maris <nikos.maris@upstreamsystems.com>
2017-01-25 17:33:57 +0200

    Revert "Update smsc.sh"
    This reverts commit 4bfa2f2a4dbdfd5d796e353f8397b46d74485775.

--- deploy/smsc.sh
+++ deploy/smsc.sh
@@ -30,19 +30,7 @@ ufw --force enable
 sed -i '/^#SYS_GID_MIN/s/^#//g' /etc/login.defs
 sed -i '/^#SYS_GID_MAX/s/^#//g' /etc/login.defs
 
-apt-get update
-apt-get -y install curl linux-image-extra-$(uname -r) linux-image-extra-virtual
-apt-get -y install apt-transport-https ca-certificates
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
-add-apt-repository \
-       "deb https://apt.dockerproject.org/repo/ \
-       ubuntu-$(lsb_release -cs) \
-       main"
-apt-get update
-
-# a known issue of docker is that it fails to start at vanilla ubuntu 16.04
-apt-get -y install docker-engine || true
-# workaround
+# ubuntu 16.04
 mkdir -p /lib/systemd/system
 echo '[Unit]' >> /lib/systemd/system/docker.service
 echo 'Description=Docker Application Container Engine' >> /lib/systemd/system/docker.service
@@ -65,8 +53,43 @@ echo 'KillMode=process' >> /lib/systemd/system/docker.service
 echo '' >> /lib/systemd/system/docker.service
 echo '[Install]' >> /lib/systemd/system/docker.service
 echo 'WantedBy=multi-user.target' >> /lib/systemd/system/docker.service
-systemctl daemon-reload
 
+apt-get update
+apt-get -y install curl linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get -y install apt-transport-https ca-certificates
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
+apt-get update
+apt-get -y install docker-engine || true
+
+# ubuntu 16.04
+mkdir -p /lib/systemd/system
+echo '[Unit]' >> /lib/systemd/system/docker.service
+echo 'Description=Docker Application Container Engine' >> /lib/systemd/system/docker.service
+echo 'Documentation=https://docs.docker.com' >> /lib/systemd/system/docker.service
+echo 'After=network.target docker.socket firewalld.service' >> /lib/systemd/system/docker.service
+echo 'Requires=docker.socket' >> /lib/systemd/system/docker.service
+echo '' >> /lib/systemd/system/docker.service
+echo '[Service]' >> /lib/systemd/system/docker.service
+echo 'Type=notify' >> /lib/systemd/system/docker.service
+echo 'EnvironmentFile=-/etc/default/docker' >> /lib/systemd/system/docker.service
+echo 'ExecStart=/usr/bin/dockerd $DOCKER_OPTS' >> /lib/systemd/system/docker.service
+echo 'ExecReload=/bin/kill -s HUP $MAINPID' >> /lib/systemd/system/docker.service
+echo 'LimitNOFILE=1048576' >> /lib/systemd/system/docker.service
+echo 'LimitNPROC=infinity' >> /lib/systemd/system/docker.service
+echo 'LimitCORE=infinity' >> /lib/systemd/system/docker.service
+echo 'TasksMax=infinity' >> /lib/systemd/system/docker.service
+echo 'TimeoutStartSec=0' >> /lib/systemd/system/docker.service
+echo 'Delegate=yes' >> /lib/systemd/system/docker.service
+echo 'KillMode=process' >> /lib/systemd/system/docker.service
+echo '' >> /lib/systemd/system/docker.service
+echo '[Install]' >> /lib/systemd/system/docker.service
+echo 'WantedBy=multi-user.target' >> /lib/systemd/system/docker.service
+
+systemctl daemon-reload
 systemctl status docker.service
 adduser user
 usermod -aG docker user
