dd50cca9ffca3d786b607f32278ec9bd1074a29c
Nicos Maris <nicos.maris@gmail.com>
2017-01-26 22:41:15 +0200

    Revert "initialize VultrAPI"
    This reverts commit 8731fa5a7f8a931d20a9ad736ca65cfff6f15b19.

--- lib/vultr.py
+++ lib/vultr.py
@@ -121,7 +121,6 @@ class Server:
         return self.ip
 
     def ready(self):
-        v = VultrAPI('token')
         try:
             while True:
                 if Delorean() - self.startuptime < timedelta(minutes=10):
