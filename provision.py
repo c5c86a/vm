58670fa1603eb964c52f924d90a441fa3c2d6455
Nicos Maris <nicos.maris@gmail.com>
2017-01-27 23:04:44 +0200

    ignore connection refused ntil reaching timeout

--- provision.py
+++ provision.py
@@ -37,8 +37,7 @@ def wait_net_service(server, port, timeout=None):
         except socket.timeout, err:
             return False
         except socket.error, err:
-            # catch timeout exception from underlying network library
-            if type(err.args) != tuple or err[0] != errno.ETIMEDOUT:
+            if type(err.args) != tuple or (err[0] != errno.ETIMEDOUT and err[0] == errno.ECONNREFUSED):
                 raise
             else:
                 eprint("waiting 10 seconds for %s to open port %d" % (server, port))
