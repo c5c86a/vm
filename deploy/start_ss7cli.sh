dc50258031d963fcaba5f876a68b75807d6afba9
Nicos Maris <nicos.maris@gmail.com>
2017-02-02 23:15:45 +0200

    start supervisor as an ubuntu service

--- deploy/start_ss7cli.sh
+++ deploy/start_ss7cli.sh
@@ -16,28 +16,49 @@ ports(){
   sed -i 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/' /etc/default/ufw
   ufw --force enable
 }
+#[unix_http_server]
+#file = /tmp/supervisor.sock
+#chmod = 0777
+#chown= mywebsiteuser:mywebsiteusergroup
+#
+#[supervisord]
+#logfile = /tmp/supervisord.log
+#logfile_maxbytes = 50MB
+#logfile_backups=10
+#loglevel = info
+#pidfile = /tmp/supervisord.pid
+#nodaemon = false
+#minfds = 1024
+#minprocs = 200
+#umask = 022
+#user = mywebsiteuser
+#identifier = supervisor
+#directory = /tmp
+#nocleanup = true
+#childlogdir = /tmp
+#strip_ansi = false
+#
+#[supervisorctl]
+#serverurl = http://localhost:9001
 
 run(){
   sudo apt-get install -y supervisor
-  echo "[program:ss7]" >> /etc/supervisor/conf.d/ss7.conf
-  echo "command=python -m SimpleHTTPServer 3435 2> /var/log/supervisord/simplehttpserver.err.log 1> /var/log/supervisord/simplehttpserver.out.log" >> /etc/supervisor/conf.d/ss7.conf
-  echo "autorestart=true" >> /etc/supervisor/conf.d/ss7.conf
-  echo "directory=/" >> /etc/supervisor/conf.d/ss7.conf
-  echo "autostart=true" >> /etc/supervisor/conf.d/ss7.conf
-  echo "autorestart=true" >> /etc/supervisor/conf.d/ss7.conf
-  echo "startretries=3" >> /etc/supervisor/conf.d/ss7.conf
-  echo "stderr_logfile=/var/log/supervisord/simplehttpserver.err.log" >> /etc/supervisor/conf.d/ss7.conf
-  echo "stdout_logfile=/var/log/supervisord/simplehttpserver.out.log" >> /etc/supervisor/conf.d/ss7.conf
-  echo "user=www-data" >> /etc/supervisor/conf.d/ss7.conf
-  echo "environment=CASSANDRA_IP='$CASSANDRA_IP'" >> /etc/supervisor/conf.d/ss7.conf
-  echo "[inet_http_server]" >> /etc/supervisor/conf.d/ss7.conf
-  echo "port = 9001" >> /etc/supervisor/conf.d/ss7.conf
-  echo "username = user # Basic auth username" >> /etc/supervisor/conf.d/ss7.conf
-  echo "password = pass # Basic auth password" >> /etc/supervisor/conf.d/ss7.conf
+  cat <<EOT >> /etc/supervisor/conf.d/ss7.conf
+[program:ss7]
+command=python -m SimpleHTTPServer 3435 2> /var/log/supervisord/simplehttpserver.err.log 1> /var/log/supervisord/simplehttpserver.out.log
+directory=/
+stderr_logfile=/var/log/supervisord/simplehttpserver.err.log
+stdout_logfile=/var/log/supervisord/simplehttpserver.out.log
+environment=CASSANDRA_IP='$CASSANDRA_IP'
 
-  sudo service supervisor start
-  supervisorctl reread
-  supervisorctl update
+[inet_http_server]
+port = 9001
+username = user
+password = pass
+EOT
+
+  supervisorctl -c /etc/supervisord.conf reload
+  supervisorctl -c /etc/supervisord.conf start ss7:*
   ps aux | grep python
 }
 
