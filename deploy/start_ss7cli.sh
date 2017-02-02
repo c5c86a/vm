fa95e5b3af80ef2f88b33879a24a69e70882408f
Nicos Maris <nicos.maris@gmail.com>
2017-02-02 23:30:04 +0200

    start supervisor as an ubuntu service

--- deploy/start_ss7cli.sh
+++ deploy/start_ss7cli.sh
@@ -6,7 +6,6 @@ ports(){
   ufw default allow outgoing
   ufw allow ssh
   ufw allow 3435/tcp # ss7 cli
-  ufw allow 9001/tcp
   ufw allow 443/tcp  # HTTPS
   # access to docker provisioner
   ufw allow 2376/tcp # docker daemon
@@ -16,50 +15,19 @@ ports(){
   sed -i 's/DEFAULT_FORWARD_POLICY="DROP"/DEFAULT_FORWARD_POLICY="ACCEPT"/' /etc/default/ufw
   ufw --force enable
 }
-#[unix_http_server]
-#file = /tmp/supervisor.sock
-#chmod = 0777
-#chown= mywebsiteuser:mywebsiteusergroup
-#
-#[supervisord]
-#logfile = /tmp/supervisord.log
-#logfile_maxbytes = 50MB
-#logfile_backups=10
-#loglevel = info
-#pidfile = /tmp/supervisord.pid
-#nodaemon = false
-#minfds = 1024
-#minprocs = 200
-#umask = 022
-#user = mywebsiteuser
-#identifier = supervisor
-#directory = /tmp
-#nocleanup = true
-#childlogdir = /tmp
-#strip_ansi = false
-#
-#[supervisorctl]
-#serverurl = http://localhost:9001
 
 run(){
-  sudo apt-get install -y supervisor
-  cat <<EOT >> /etc/supervisor/conf.d/ss7.conf
-[program:ss7]
-command=python -m SimpleHTTPServer 3435 2> /var/log/supervisord/simplehttpserver.err.log 1> /var/log/supervisord/simplehttpserver.out.log
-directory=/
-stderr_logfile=/var/log/supervisord/simplehttpserver.err.log
-stdout_logfile=/var/log/supervisord/simplehttpserver.out.log
-environment=CASSANDRA_IP='$CASSANDRA_IP'
+  cat <<EOT >> /usr/lib/systemd/system/ss7.service
+[Unit]
+Description=ss7
 
-[inet_http_server]
-port = 9001
-username = user
-password = pass
+[Service]
+ExecStart=python -m SimpleHTTPServer 3435
 EOT
 
-  supervisorctl -c /etc/supervisord.conf reload
-  supervisorctl -c /etc/supervisord.conf start ss7:*
-  ps aux | grep python
+  systemctl enable ss7
+  systemctl start ss7
+  systemctl status ss7
 }
 
 echo "curl -X GET http://$CASSANDRA_IP:9042"
