dddceec57b4ad0c150b7abcd287895311411eb89
Nicos Maris <nicos.maris@gmail.com>
2017-02-14 18:00:18 +0200

    .

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Updates branch gh-pages
+# Github serves that branch as a static site at https://nicosmaris.github.io/js
+rev=$(git rev-parse --short HEAD)
+
+mkdir -p _site
+cp -R app _site/
+cp index.html _site/
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
+cd _site
+
+git init
+git config user.name "Nicos Maris"
+git config user.email "nicos.maris@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/nicosmaris/vm.git"
+git fetch upstream && git reset upstream/smsc
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:smsc
