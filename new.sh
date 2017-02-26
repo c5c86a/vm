afa1f4cd185bbe47bc5c1321d766e98ff0559d38
Nicos Maris <nicos.maris@gmail.com>
2017-02-26 02:21:01 +0200

    same with payload at POST data

--- new.sh
+++ new.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl -X POST -H "API-key: $(cat token)" https://api.vultr.com/v1/sshkey/create?name=tmp&sshkey="$(cat otp.pub)"
+#curl -X POST -H "API-key: $(cat token)" https://api.vultr.com/v1/sshkey/create?name=tmp&sshkey="$(cat otp.pub)"
+curl -X POST -H "API-key: $(cat token)" --data "name='tmp'" --data "sshkey=$(cat otp.pub)" https://api.vultr.com/v1/sshkey/create
 
 #curl --header "API-key: $(cat token)" https://api.vultr.com/v1/sshkey/list
