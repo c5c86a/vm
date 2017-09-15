66b551c9703cfc0deba7252f097978175b221ce4
Nikos Maris <nmaris@atypon.com>
2017-09-15 12:08:13 +0300

    install saltstack and add it to travis cache

---
+++ provisioner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+install_saltstack(){
+curl -L https://bootstrap.saltstack.com -o install_salt.sh
+sudo sh install_salt.sh -P -M -L # The -L option will install salt-cloud
+}
+
+install_saltstack
+
